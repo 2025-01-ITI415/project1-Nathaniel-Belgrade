05bc1d7e813b4016b0322d47e0c61a95108a5980
Nathaniel-Belgrade <nebelgrade@gmail.com>
2025-03-01 23:40:19 -0500

    Whack A Mole 1.0
    Moles now spawn on each wall instead of only the north wall!!!

--- Assets/__Scripts/GameController.cs
+++ Assets/__Scripts/GameController.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
 
     public GameObject mole;
+    public GameObject Wall;
     public float radius;
     public float spawnrate = 3f;
+    private Vector3 randomPos;
+    private float xCoord;
+    private float yCoord;
+    private float zCoord;
 
     private void Start()
     {
@@ -17,8 +23,37 @@ public class GameController : MonoBehaviour
 
     public void SpawnMole()
     {
-        Vector3 randompos = Random.insideUnitCircle * radius;
-        Instantiate(mole, randompos, Quaternion.identity);
-        Invoke("SpawnMole", spawnrate);
+        switch (Wall.tag)
+        {
+            case ("North"):
+                xCoord = Random.Range(-8.9f, 8.8f);
+                yCoord = Random.Range(-3.9f, 5.6f);
+                randomPos.Set(xCoord, yCoord, 0.35f);
+                Instantiate(mole, randomPos, Quaternion.identity);
+                Invoke("SpawnMole", spawnrate);
+                break;
+            case ("South"):
+                xCoord = Random.Range(-8.9f, 8.8f);
+                yCoord = Random.Range(-3.9f, 5.6f);
+                randomPos.Set(xCoord, yCoord, -19.6f);
+                Instantiate(mole, randomPos, Quaternion.identity);
+                Invoke("SpawnMole", spawnrate);
+                break;
+            case ("East"):
+                yCoord = Random.Range(-4f, 5.85f);
+                zCoord = Random.Range(-18.3f, -1.2f);
+                randomPos.Set(10f, yCoord, zCoord);
+                Instantiate(mole, randomPos, Quaternion.identity);
+                Invoke("SpawnMole", spawnrate);
+                break;
+            case ("West"):
+                yCoord = Random.Range(-4f, 5.85f);
+                zCoord = Random.Range(-18.3f, -1.2f);
+                randomPos.Set(-10f, yCoord, zCoord);
+                Instantiate(mole, randomPos, Quaternion.identity);
+                Invoke("SpawnMole", spawnrate);
+                break;
+
+        }
     }
 }
